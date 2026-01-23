736b30f57e191b51c0a9757572357d354aeeb379
Wedsonlin <1090319538@qq.com>
2026-01-23 16:54:58 +0800

    implement SwiGLU

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -22,5 +22,41 @@ class Embedding(nn.Module):
         nn.init.trunc_normal_(self.embedding_matrix, 0, 1, -3, 3)
 
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+        '''
+            X=(x_1,x_2,...): LongTensor, W: Tensor
+            W[X] -> 用X中的每个元素x_i去索引W,得(W[x_1],W[x_2],...)
+        '''
         return self.embedding_matrix[token_ids]
             
+class RMSNorm(nn.Module):
+    def __init__(self, d_model: int, eps: float=1e-5, device=None, dtype=None):
+        super().__init__()
+        self.g = nn.Parameter(torch.ones(d_model, device=device, dtype=dtype))
+        self.eps = eps
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        RMS_x = torch.rsqrt(x.pow(2).mean(-1,keepdim=True) + self.eps)
+        x = x * RMS_x * self.g
+        return x.to(in_dtype)
+    
+class SwiGLU(nn.Module):
+    def __init__(self, d_model: int, d_ff: int=None, device=None, dtype=None):
+        super().__init__()
+        if not d_ff:
+            d_ff = round(8/3 * d_model / 64) * 64
+        self.W1 = nn.Parameter(torch.empty(d_ff,d_model,device=device,dtype=dtype))
+        self.W2 = nn.Parameter(torch.empty(d_model,d_ff,device=device,dtype=dtype))
+        self.W3 = nn.Parameter(torch.empty(d_ff,d_model,device=device,dtype=dtype))
+
+        sigma = sqrt(2/(d_ff+d_model))
+        nn.init.trunc_normal_(self.W1, 0, sigma, -3*sigma, 3*sigma)
+        nn.init.trunc_normal_(self.W2, 0, sigma, -3*sigma, 3*sigma)
+        nn.init.trunc_normal_(self.W3, 0, sigma, -3*sigma, 3*sigma)
+    
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        W1x = einsum(self.W1, x, "d_ff d_model, ... d_model -> ... d_ff")
+        W3x = einsum(self.W3, x, "d_ff d_model, ... d_model -> ... d_ff")
+        SiLU_W1x = W1x*torch.sigmoid(W1x)
+        return einsum(self.W2, SiLU_W1x*W3x, "d_model d_ff, ... d_ff -> ... d_model")
--- tests/adapters.py
+++ tests/adapters.py
@@ -10,7 +10,7 @@ from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
 from cs336_basics.bpe import train_bpe,Tokenizer
-from cs336_basics.model import Linear, Embedding
+from cs336_basics.model import Linear, Embedding, RMSNorm, SwiGLU
 
 
 def run_linear(
@@ -88,7 +88,13 @@ def run_swiglu(
     # swiglu.w1.weight.data = w1_weight
     # swiglu.w2.weight.data = w2_weight
     # swiglu.w3.weight.data = w3_weight
-    raise NotImplementedError
+    swiglu = SwiGLU(d_model,d_ff)
+    swiglu.load_state_dict({
+        "W1":w1_weight,
+        "W2":w2_weight,
+        "W3":w3_weight
+    })
+    return swiglu(in_features)
 
 
 def run_scaled_dot_product_attention(
@@ -383,7 +389,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    rmsnorm = RMSNorm(d_model,eps)
+    rmsnorm.load_state_dict({"g":weights})
+    return rmsnorm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
