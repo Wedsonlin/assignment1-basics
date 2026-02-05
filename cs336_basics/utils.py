02ba33342d22fcd56ac65d20492193f062477b9a
Wedsonlin <1090319538@qq.com>
2026-02-06 00:54:22 +0800

    decoder implemented

--- cs336_basics/utils.py
+++ cs336_basics/utils.py
@@ -81,8 +81,56 @@ def load_checkpoint(src: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
 
     return obj['iteration']
 
-def decoding(model: torch.nn.Module, prompt: torch.Tensor, max_generate_tokens: int, 
-             temperature: float=1.0, sampling_threshold: float=0.0) -> torch.Tensor:
-    
+def decoding(model: torch.nn.Module, prompt: torch.Tensor, eos_token_id:int, max_generate_tokens: int, 
+             temperature: float=1.0, sampling_threshold: float=1.0, device: typing.Optional[torch.device] = None) -> torch.Tensor:
+    '''
+        prompt shape: (1,T) or (,T)
+    '''
+    if prompt.dim() == 1:
+        prompt = prompt.unsqueeze(0) # (T,) -> (1,T)
 
-    return
+    if device is None:
+        device = next(model.parameters()).device()
+    input = prompt.to(device)
+
+    for _ in range(max_generate_tokens):
+        logits = model(input)
+
+        if logits.dim() == 3: # (1,T,V)
+            next_logits = logits[0,-1,:]
+        else: # (T,V)
+            next_logits = logits[-1,:]
+
+        # temperature-scaled logits
+        if temperature == 0:
+            idx = next_logits.argmax()
+            probs = torch.zeros_like(next_logits)
+            probs[idx] = 1.0
+        else:
+            probs = softmax(next_logits/temperature,-1)
+
+        # top-p sampling (nucleus sampling)
+        if sampling_threshold < 1.0:
+            sorted_probs, sorted_idx = torch.sort(probs, dim=-1, descending=True)
+            cumprobs = torch.cumsum(sorted_probs, dim=-1)
+
+            remove = cumprobs > sampling_threshold
+            remove[1:] = remove[:-1].clone()
+            remove[0] = False
+
+            remove_mask = torch.zeros_like(probs, device=probs.device,dtype=torch.bool).scatter(
+                dim=-1, index=sorted_idx, src=remove
+            )
+
+            probs = probs.masked_fill(remove_mask, 0) # 将True的地方替换为0
+            probs = probs / probs.sum(dim=-1)
+    
+        next_id = torch.multinomial(probs, num_samples=1)
+        next_token = torch.tensor([[next_id]], device=device, dtype=input.dtype)
+
+        input = torch.cat([input, next_token],dim=1)
+        
+        if next_id == eos_token_id:
+            break
+
+    return input.unsqueeze(0)
